ab64e8dddfe91b56f8d02ce05bcd2fc730a30ef8
ASWIN A K <007aswinak007@gmail.com>
2022-05-01 03:13:24 -0400

    aswin

--- login_page/login.php
+++ login_page/login.php
@@ -3,20 +3,20 @@ $names=$_POST['name'];
 $email=$_POST['email'];
 $password2=$_POST['password2'];
 
-$servername = "localhost";
+$servername = "127.0.0.1";
 $username = "root";
 $password = "";
-$dbname = "Udetails";
+$dbname = "test";
 
 // Create connection
-$conn = mysqli_connect('localhost', 'root','','Udetails');
+$conn = mysqli_connect('127.0.0.1','root','','test');
 //$db= mysql_select_db('$conn);
 // Check connection\
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "INSERT INTO 'Udeatils'.'users' ('names', 'password2', 'email') VALUES ('$names','$password2','$email')";
+$sql = "INSERT INTO test (names, password2, email) VALUES ('$names','$password2','$email')";
 
 if (mysqli_query($conn, $sql)) {
   echo "New record created successfully";
