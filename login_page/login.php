4daf143e1117b108f7e61d113318ef9447262d1c
Amal krishna M.U <83550433+amal-krishna-m-u@users.noreply.github.com>
2022-05-01 14:36:47 +0530

    change

--- login_page/login.php
+++ login_page/login.php
@@ -11,23 +11,16 @@ $password = "";
 $dbname = "test";
 
 // Create connection
-<<<<<<< HEAD
+
 $conn = mysqli_connect('$severname','$username','','$dbname');
-=======
-$conn = mysqli_connect('127.0.0.1','root','','test');
->>>>>>> ab64e8dddfe91b56f8d02ce05bcd2fc730a30ef8
-//$db= mysql_select_db('$conn);
+
 // Check connection\
 if (!$conn)
  {
   die("Connection failed: " . mysqli_connect_error());
 }
+$sql = "INSERT INTO users ('',names, password2, email) VALUES ('$names','$password2','$email')";
 
-<<<<<<< HEAD
-$sql = "INSERT INTO users ('',names, password2, email') VALUES ('$names','$password2','$email')";
-=======
-$sql = "INSERT INTO test (names, password2, email) VALUES ('$names','$password2','$email')";
->>>>>>> ab64e8dddfe91b56f8d02ce05bcd2fc730a30ef8
 
 if (mysqli_query($conn, $sql)) {
   echo "New record created successfully";
