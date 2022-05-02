f4cefff250c4571388968c1c3ec77e47a8f7ca09
ASWIN A K <007aswinak007@gmail.com>
2022-05-02 03:33:10 -0400

    commited by amal

--- login_page/reg.php
+++ login_page/reg.php
@@ -12,14 +12,28 @@ $dbname = "Udetails";
 
 // Create connection
 
-$conn = mysqli_connect('127.0.0.1','root','','Udetails');
+$conn = mysqli_connect('127.0.0.1','root','','test');
 
 // Check connection\
 if (!$conn)
  {
   die("Connection failed: " . mysqli_connect_error());
 }
-$sql = "INSERT INTO users ('',names, password2, email) VALUES ('$names','$password2','$email')";
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+  // username and password sent from form 
+  
+  $myemail = mysqli_real_escape_string($conn,$_POST['email']); 
+  
+  $sql1 = "SELECT id FROM test WHERE  email= '$myemail'";
+  $result = mysqli_query($conn,$sql1);
+  $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+  
+  $count = mysqli_num_rows($result);
+  if($count== 0){
+
+$sql = "INSERT INTO test(names, password2, email) VALUES ('$names','$password2','$email')";
 
 
 if (mysqli_query($conn, $sql)) {
@@ -29,5 +43,9 @@ exit();
   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
-mysqli_close($conn);
+mysqli_close($conn);}
+else
+{
+  header("location:login.html");
+}}
 ?>
