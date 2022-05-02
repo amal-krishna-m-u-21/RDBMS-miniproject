d7c8d04898464c6c08a55a2a02a93d848a1c62c7
ASWIN A K <007aswinak007@gmail.com>
2022-05-02 03:32:34 -0400

    checking email in db b4 registration

--- login_page/login.php
+++ login_page/login.php
@@ -1,26 +1,25 @@
 <?php
   
-   $db = mysqli_connect('127.0.0.1','root','',test);
+   $db = mysqli_connect('127.0.0.1','root','','test');
    session_start();
   
 
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form 
       
-      $myeamil = mysqli_real_escape_string($db,$_POST['email']);
+      $myemail = mysqli_real_escape_string($db,$_POST['email']);
       $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
       
       $sql = "SELECT id FROM test WHERE  email= '$myemail' and password2 = '$mypassword'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
       
       $count = mysqli_num_rows($result);
       
       // If result matched $myeamil and $mypassword, table row must be 1 row
 		
       if($count == 1) {
-         session_register("myemail");
+        // session_register("myemail");
          $_SESSION['login_user'] = $myemail;
          header("location:dashboard.html");
       }else 
