7f0ec7d878179ab219fd5b9db25c6880817c6926
Amal <amlkrishnam3@gmail.com>
2022-06-22 00:06:12 +0530

    admin can add interest

--- Admin_dashboard/sudo.php
+++ Admin_dashboard/sudo.php
@@ -10,29 +10,46 @@
          <h1>Welcome Boss<br>
         
          </h1> 
-        </hearder>
-        <?php 
+      </hearder>
+      <?php 
          session_start();
          include('session.php');
-       $ls=$_SESSION['login_session'];
-       //fetching details
-       $sql="SELECT * FROM User_details WHERE user_id = $ls";
-       $result = mysqli_query($conn,$sql);
-       $row = mysqli_fetch_assoc($result);
-
-      // getting values from form
-
-       $interest=$_POST['interest'];
-       //creating table if table dosen't exist
-
-
-       include('add_interest.php'); 
-
+         $ls=$_SESSION['login_session'];
+         //fetching details
+         $sql="SELECT * FROM User_details WHERE user_id = $ls";
+         $result = mysqli_query($conn,$sql);
+         $row = mysqli_fetch_assoc($result);
+         include('add_interest.php');
          ?> 
-         
+  
+  
 
           
       <h2><a href = "../Login_page/logout.php">Sign Out</a></h2>
    </body>
     
 </html>
+         <?php
+      // getting values from form
+
+       $interest=$_POST['interest'];
+       //creating table if table dosen't exist
+       if($interest != ''){
+       $sql1 = "INSERT INTO Interest (interest_name) VALUES 
+       ('$interest')";
+       
+       
+       if(mysqli_query($conn,$sql1))
+                                     {
+                                       $flag="inserted";
+                                       exit();
+                                      }
+                                       else
+                                             {
+                                               echo "<br> Error : ". "<br>" . mysqli_error($conn) ."<br>"."Insertion problem";
+                                               exit();
+                                              }
+
+                                             }
+                                              
+                                              ?> 
