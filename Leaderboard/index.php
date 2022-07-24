5dee6d3b8654af51223f245de29ccfe5dce38f0f
Amal <amlkrishnam3@gmail.com>
2022-07-24 21:01:17 +0530

    leaderboard

--- Leaderboard/index.php
+++ Leaderboard/index.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<head>
+<link rel="stylesheet" href="./Css/lb.css">
+
+</head>
+<body>
+    
+
+
+
 <?php
 include('config.php');
 include('session.php');
@@ -10,12 +20,18 @@ include('web_updation.php');
 $interest_id= $_POST['interest'];
 //echo $interest_id;
 //echo $user_id;
-echo"<h1> MOBILE APPLIATIONS</h1>";
 
+
+?> 
+
+<div class="card">   
+<?php
 include('app_leaderboard.php');
 
+?></div>
 
-echo"<h1> WEB-PAGES </h1>";
+<div class="card"> 
+<?php
 include('web_leaderboard.php');
 
 
@@ -23,3 +39,13 @@ include('web_leaderboard.php');
 
 
 ?>
+</div>
+
+ <!-- animation  jquery library linking :Anime.js  -->
+ <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
+         <script src='https://cdn.jsdelivr.net/npm/animejs@3.1.0/lib/anime.js'></script><script  src="./Js/reg-animation.js"></script>
+
+
+
+</body>
+</html>
--- Login_page/index.php
+++ Login_page/index.php
@@ -11,7 +11,7 @@ session_start();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Let's Go</title>
-  <link rel="stylesheet" href="./Css/login.css">
+<link rel="stylesheet" href="./Css/login.css"> 
    <script src="./Js/reg-validation.js"></script>
 
 </head>
