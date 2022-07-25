8aa408285eadf0061de3d69ff40be927aab17ba0
Amal <amlkrishnam3@gmail.com>
2022-07-25 18:37:23 +0530

    updates

--- Admin_dashboard/add_interest.php
+++ Admin_dashboard/add_interest.php
@@ -1,15 +1,16 @@
 
-<div class = "card"><br><br><br><br><br><br><br><br><br><br>
+
+
+<div class="card"></div>
+<div class = "card">
      <h2><i> Enter new interest</i></h2><br>
      <p>Enter Interest that need to be added to the database</p>
           <form action = "sudo.php" method="post" >
 
           <div class="group">
-   <input type="text" name="interest" id="interest" placeholder="interest">
+   <input type="text" name="interest" id="interest" placeholder="Input the Interest">
 </div>
-
     <input class="button" type="submit" value= "ADD">
-
 
 </div>
          </form>
