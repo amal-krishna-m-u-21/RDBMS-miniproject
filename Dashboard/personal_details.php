6f22093c39e24ac06b887842c756aab026ce49da
Amal <amlkrishnam3@gmail.com>
2022-07-25 13:06:14 +0530

    dashboard update

--- Dashboard/personal_details.php
+++ Dashboard/personal_details.php
@@ -5,6 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .divScroll {
+            overflow: scroll;
+            height: 10px;
+            width: 10px;
+            margin-left: 10px;
+
+            ;
+
+        }
+    </style>
 </head>
 <body>
     <header><h1>User Details</h1></header>
@@ -23,20 +34,97 @@ echo mysqli_error($conn);
 }
         ?>
 <header>
-    <label>User Name:</label><?= $details['user_name'];?>
-    <br><br>
-    <label>User E-mail:</label><?= $details['email']; ?>
-    <br><br>
-    <label>User's DOB:</label><?= $details["dob"]; ?>
+    <label>User Name:</label> 
+</header>
+    <h5><?= $details['user_name'];?>
+</h5>
+  <header> 
+    <label>User E-mail:</label>
+    </header> 
+    <h5><?= $details['email']; ?></h5>
+
+<header>   
+     <label>User's DOB:</label><br>
+</header>
+    <h5><?= $details["dob"]; ?></h5>
 
     
-</header>
 
         <?php
  
 
 
         ?>
+        <br><br><br>
+        <div class="divScroll"> 
+<header><h1>List item</h1></header><br>
+<header> <label>Select type:</label><br>
+<form action="add.php" method="post">
+    <select name="type" id="type">
+        <option value="select">slect</option>
+        <option value="1">Mobile APP</option>
+        <option value="2">Web Page</option>
+    </select><br><br>
+    <label>Enter URL</label><br>
+    <input type="url" name="item" id="item">
+
+    <h3>Review and Rating</h3><br>
+    <textarea rows="5" cols="20" name="review"  placeholder="Write review here" required>
+</textarea>
+
+<div class="group"> 
+
+<div class="slidecontainer">
+  <input type="range" min="0" max="10" value="0" name="rating"class="slider" id="myRange" required></div>
+  <div class="group"> 
+<p>Rating: <span id="demo"></span></p>
+</div>
+</div>
+
+<script>
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+</script>
+<input class="button" type="submit" value="Done">
+</form>
+<br>
+<br>
+<br>
+<br>
+
+
+
+<br>
+<br>
+<br>
+<br>
+
+<br>
+<br>
+<br>
+<br>
+
+<br>
+<br>
+<br>
+<br>
+
+<br>
+<br>
+<br>
+<br>
+
+
+</header>
+
+</div>
+
+
 
 
 </body>
--- Dashboard/review_details.php
+++ Dashboard/review_details.php
@@ -3,8 +3,10 @@
 <style>
         .divScroll {
             overflow: scroll;
-            height: 400px;
-            width: 570px;
+            height: 10px;
+            width: 10px;
+            margin-left: 10px;
+
             ;
 
         }
@@ -34,7 +36,7 @@ else{
     echo "error".mysqli_error($conn);
 }
 
-?></div>
+?>
 
 
 
@@ -44,8 +46,8 @@ else{
 $query="SELECT Web.web_name ,Rating.rating,Rating.review FROM Rating JOIN Web ON Rating.web_id=Web.web_id WHERE Rating.user_id= $ls ";
 $result=mysqli_query($conn,$query);
 if($result)
-{ ?><h3>Mobile Appliations</h3>
-<div class="divScroll">  <?php
+{ ?><h3>WEb -Appliations</h3>
+ <?php
 foreach($result as $li){
 ?>
 <header><?=$li['web_name']; ?></header>
@@ -60,47 +62,8 @@ else{
     echo "error".mysqli_error($conn);
 }
 
-?></div>
-
-<header>List item</header>
-<header> 
-<form action="add.php" method="post">
-    <select name="type" id="type">
-        <option value="select">slect</option>
-        <option value="1">Mobile APP</option>
-        <option value="2">Web Page</option>
-    </select>
-    <input type="url" name="item" id="item">
-
-    <h3>Review and Rating</h3>
-    <textarea rows="5" cols="20" name="review"  placeholder="Write review here" required>
-</textarea>
-
-<div class="group"> 
-
-<div class="slidecontainer">
-  <input type="range" min="0" max="10" value="0" name="rating"class="slider" id="myRange" required></div>
-  <div class="group"> 
-<p>Rating: <span id="demo"></span></p>
+?>
 </div>
-</div>
-
-<script>
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
-
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
-</script>
-<input class="button" type="submit" value="Done">
-</form>
-
-
-</header>
-
-
 
 </body>
 </html>
